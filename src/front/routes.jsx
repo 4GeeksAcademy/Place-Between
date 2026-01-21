991208412ad20e2f24107391b6e68c71695f509d
Daniel Hernández <d.hernandezt.96@gmail.com>
2026-01-21 17:47:31 +0000

    Arreglos de Nav, footer y rutas. Se aniaden placeholders para la app

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,28 +1,39 @@
-// Import necessary components and functions from react-router-dom.
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-import { Layout } from "./pages/Layout";
+import { PublicLayout } from "./pages/PublicLayout";
+import { AppLayout } from "./pages/AppLayout";
+
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
-import { Today } from "./pages/Today";
 
+import { Today } from "./pages/Today";
+import { Activities } from "./pages/Activities";
+import { Mirror } from "./pages/Mirror";
+import { Profile } from "./pages/Profile";
 
 export const router = createBrowserRouter([
+  // Público
   {
     path: "/",
-    element: <Layout />,
+    element: <PublicLayout />,
     children: [
       { path: "/", element: <Home /> },
-
-      // Auth
       { path: "/auth/login", element: <Login /> },
       { path: "/auth/signup", element: <Signup /> },
+    ],
+  },
 
-      // App
+  // App
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
       { path: "/today", element: <Today /> },
-
+      { path: "/activities", element: <Activities /> },
+      { path: "/mirror", element: <Mirror /> },
+      { path: "/profile", element: <Profile /> },
     ],
   },
 ]);
