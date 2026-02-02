126251b8377a0e7edffeed546459b0bc1362810f
santiago-pp <santiagopereaap@gmail.com>
2026-02-02 22:46:21 +0100

    Add MusicPlayer component for SoundCloud integration
    Implement a MusicPlayer component that embeds a SoundCloud player.

---
+++ src/front/components/MusicPlayer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const MusicPlayer = ({ url }) => {
+    if (!url) return null;
+
+    const embedPlayerUrl = `https://w.soundcloud.com/player/?url=${encodeURIComponent(
+        url)}&auto_play=true&loop=true&hide_related=true&show_comments=false&show_user=false&show_reposts=false&visual=false`;
+
+
+    return (
+        <iframe
+            src={embedPlayerUrl}
+            allow="autoplay"
+            style={{ width: 1, height: 1, opacity: 0 }}
+            title="default-music"
+        />
+    );
+};
